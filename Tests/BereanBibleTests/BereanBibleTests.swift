08a646d3c445590e402aad40584565ca885aff07
C. Bess <cbess@users.noreply.github.com>
2023-08-16 23:04:57 -0500

    add ot/nt test

--- Tests/BereanBibleTests/BereanBibleTests.swift
+++ Tests/BereanBibleTests/BereanBibleTests.swift
@@ -2,8 +2,23 @@ import XCTest
 @testable import BereanBible
 
 final class BereanBibleTests: XCTestCase {    
-    func testVerse() throws {
-        let text = BereanBibleManager().text(book: 1, chapter: 1, verseRange: Range(uncheckedBounds: (1, 1)))
+    func testOTVerse() throws {
+        // english
+        var text = BereanBibleManager().text(book: 1, chapter: 1, verseRange: Range(uncheckedBounds: (1, 1)))
         XCTAssertEqual(text, "In the beginning God - created the heavens and the earth.")
+        
+        // hebrew
+        text = BereanBibleManager().text(book: 1, chapter: 1, verseRange: Range(uncheckedBounds: (1, 1)), isOrig: true)
+        XCTAssertEqual(text, "בְּרֵאשִׁ֖ית אֱלֹהִ֑ים אֵ֥ת בָּרָ֣א הַשָּׁמַ֖יִם וְאֵ֥ת הָאָֽרֶץ׃")
+    }
+    
+    func testNTVerse() throws {
+        // english
+        var text = BereanBibleManager().text(book: 43, chapter: 1, verseRange: Range(uncheckedBounds: (1, 1)))
+        XCTAssertEqual(text, "In [the] beginning was the Word, and the Word was with - God, and the Word was God.")
+        
+        // hebrew
+        text = BereanBibleManager().text(book: 43, chapter: 1, verseRange: Range(uncheckedBounds: (1, 1)), isOrig: true)
+        XCTAssertEqual(text, "Ἐν ἀρχῇ ἦν ὁ Λόγος καὶ ὁ Λόγος ἦν πρὸς τὸν Θεόν καὶ ὁ Λόγος ἦν Θεὸς")
     }
 }
