651dcf2a972b171355a3e3e73f83230c3aaf53a6
C. Bess <cbess@users.noreply.github.com>
2023-08-17 19:05:18 -0500

    support verse parts - update tests

--- Sources/BereanBible/BereanBible.swift
+++ Sources/BereanBible/BereanBible.swift
@@ -5,9 +5,9 @@ import SQLite
 let interlinearTable = Table("interlinear")
 let strongsTable = Table("strongs")
 // columns
-let origSort = Expression<Int>("orig_sort")
+let origSort = Expression<Double>("orig_sort")
 let origText = Expression<String>("orig_text")
-let bsbSort = Expression<Int>("bsb_sort")
+let bsbSort = Expression<Double>("bsb_sort")
 let bsbText = Expression<String>("bsb_text")
 let langCode = Expression<String>("lang_code")
 let bookId = Expression<Int>("book")
@@ -30,10 +30,45 @@ public struct BereanBibleManager {
 
     /// Returns the text for the specified range, or the given chapter for the original language or the BSB translation (default)
     public func text(book: Int, chapter: Int, verseRange: Range<Int>?, isOrig: Bool = false) -> String {
-        let col = isOrig ? origText : bsbText
+        let verses = verses(book: book, chapter: chapter, verseRange: verseRange, isOrig: isOrig)
         
+        // build the full text value from all the rows/verses
+        let fullText = NSMutableString()
+        for verse in verses {
+            // sort the parts, based on language
+            let parts = verse.parts.sorted { lhs, rhs in
+                return isOrig ? lhs.origSort < rhs.origSort : lhs.sort < rhs.sort
+            }
+            
+            for part in parts {
+                let text = isOrig ? part.origText : part.text
+                fullText.append(text + " ")
+            }
+        }
+        
+        return (fullText as String).trimmingCharacters(in: .whitespaces)
+    }
+    
+    private func getVersePart(from row: Row) -> VersePart {
+        return VersePart(
+            origSort: try! row.get(origSort),
+            origText: try! row.get(origText),
+            sort: try! row.get(bsbSort),
+            text: try! row.get(bsbText),
+            bookId: try! row.get(bookId),
+            chapter: try! row.get(chapterId),
+            verse: try! row.get(verseId),
+            transliteration: try! row.get(translit),
+            parsing: try! row.get(parsing),
+            parsingFull: try! row.get(parsingFull),
+            strongs: try! row.get(strongsId),
+            langCode: try! row.get(langCode)
+        )
+    }
+    
+    public func verses(book: Int, chapter: Int, verseRange: Range<Int>?, isOrig: Bool = false) -> [Verse] {
         // select the verses
-        var table = interlinearTable.select(col).filter(bookId == book).filter(chapterId == chapter)
+        var table = interlinearTable.filter(bookId == book).filter(chapterId == chapter)
         if let range = verseRange {
             if range.startIndex == range.endIndex {
                 table = table.filter(verseId == range.startIndex)
@@ -44,16 +79,57 @@ public struct BereanBibleManager {
         
         let query = try? db.prepare(table)
         guard let query = query else {
-            return ""
+            return []
         }
         
-        // build the full text value from all the rows/verses
-        let fullText = NSMutableString()
+        var verses: [Verse] = []
+        var parts: [VersePart] = []
+        
+        // get the verses
+        var lastVerseId = 0
         for row in query {
-            let text = try! row.get(col)
-            fullText.append(text + " ")
+            let verseId = try! row.get(verseId)
+            
+            // wait until the parts for the first verse in the range are aggregated
+            if lastVerseId == 0 {
+                lastVerseId = verseId
+            }
+            
+            // when the verse changes, store the parts
+            if verseId != lastVerseId {
+                verses.append(Verse(bookId: book, chapter: chapter, verse: lastVerseId, parts: parts, langCode: parts.first!.langCode))
+                parts = []
+                lastVerseId = verseId
+            }
+            
+            parts.append(getVersePart(from: row))
         }
         
-        return (fullText as String).trimmingCharacters(in: .whitespaces)
+        // store the last parts
+        verses.append(Verse(bookId: book, chapter: chapter, verse: lastVerseId, parts: parts, langCode: parts.first!.langCode))
+        return verses
     }
+}
+
+public struct Verse {
+    var bookId: Int
+    var chapter: Int
+    var verse: Int
+    var parts: [VersePart]
+    var langCode: String
+}
+
+public struct VersePart {
+    var origSort: Double
+    var origText: String
+    var sort: Double
+    var text: String
+    var bookId: Int
+    var chapter: Int
+    var verse: Int
+    var transliteration: String
+    var parsing: String
+    var parsingFull: String
+    var strongs: Int
+    var langCode: String
 }
--- Tests/BereanBibleTests/BereanBibleTests.swift
+++ Tests/BereanBibleTests/BereanBibleTests.swift
@@ -9,7 +9,7 @@ final class BereanBibleTests: XCTestCase {
         
         // hebrew
         text = BereanBibleManager().text(book: 1, chapter: 1, verseRange: Range(uncheckedBounds: (1, 1)), isOrig: true)
-        XCTAssertEqual(text, "בְּרֵאשִׁ֖ית אֱלֹהִ֑ים אֵ֥ת בָּרָ֣א הַשָּׁמַ֖יִם וְאֵ֥ת הָאָֽרֶץ׃")
+        XCTAssertEqual(text, "בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים אֵ֥ת הַשָּׁמַ֖יִם וְאֵ֥ת הָאָֽרֶץ׃")
     }
     
     func testNTVerse() throws {
@@ -19,6 +19,6 @@ final class BereanBibleTests: XCTestCase {
         
         // hebrew
         text = BereanBibleManager().text(book: 43, chapter: 1, verseRange: Range(uncheckedBounds: (1, 1)), isOrig: true)
-        XCTAssertEqual(text, "Ἐν ἀρχῇ ἦν ὁ Λόγος καὶ ὁ Λόγος ἦν πρὸς τὸν Θεόν καὶ ὁ Λόγος ἦν Θεὸς")
+        XCTAssertEqual(text, "Ἐν ἀρχῇ ἦν ὁ Λόγος καὶ ὁ Λόγος ἦν πρὸς τὸν Θεόν καὶ Θεὸς ἦν ὁ Λόγος")
     }
 }
